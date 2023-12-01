7f9a2547abb044614052e7554ba775c06fed69a0
shobindiran <shobindiran@gmail.com>
2023-12-01 16:48:01 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useState } from "react";
+
 
 function App() {
+
+  const [users,setUsers] = useState([])
+
+  useEffect(()=>{
+    const fetchData = async()=>{
+      try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        setUsers(data)
+      }
+      catch(err){
+        console.log(err)
+      }
+    }
+
+    fetchData();
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <h4>{user.id}</h4>
+            <h5>{user.name}</h5>
+            <h4>City</h4>
+            <h5>{user.address.city}</h5>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
